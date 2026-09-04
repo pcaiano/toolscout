f667817f3e48d1b36f8048c79e3df0628e46d974
pcaiano <pcaiano@gmail.com>
2026-09-04 15:09:16 +0100

    Route protected Command Center stats

--- command-center-entry-worker.js
+++ command-center-entry-worker.js
@@ -3,9 +3,15 @@ import base from './affiliate-workflow-worker.js';
 function normalizeCommandCenterRequest(request) {
   if (request.method !== 'GET') return request;
   const url = new URL(request.url);
-  if (url.pathname !== '/analytics' && url.pathname !== '/analytics/') return request;
-  url.pathname = '/analytics.html';
-  return new Request(url.toString(), request);
+  if (url.pathname === '/analytics' || url.pathname === '/analytics/') {
+    url.pathname = '/analytics.html';
+    return new Request(url.toString(), request);
+  }
+  if (url.pathname === '/analytics/api/stats') {
+    url.pathname = '/api/stats';
+    return new Request(url.toString(), request);
+  }
+  return request;
 }
 
 export default {
