586ac0afd14a71f36b25403bc9bab872384f2fa5
pcaiano <pcaiano@gmail.com>
2026-09-09 10:38:48 +0100

    Wire guarded distribution throughput layer

--- distribution-orchestrator-worker.js
+++ distribution-orchestrator-worker.js
@@ -1,4 +1,4 @@
-import base from './distribution-autonomous-worker.js';
+import base from './distribution-throughput-worker.js';
 import {distributionSurfaceMetrics} from './distribution-impact-worker.js';
 
 const H={'Content-Type':'application/json; charset=UTF-8','Cache-Control':'no-store'};
