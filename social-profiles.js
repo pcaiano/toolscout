e5b5ef46edd382ac5bf67e67791c78b89d1dd5c0
pcaiano <pcaiano@gmail.com>
2026-09-24 13:43:06 +0100

    Add ToolScout DEV Community profile

--- social-profiles.js
+++ social-profiles.js
@@ -1,7 +1,8 @@
 export const TOOLSCOUT_SOCIAL_PROFILES=Object.freeze([
   {key:'linkedin',label:'LinkedIn',url:'https://www.linkedin.com/company/146229906/'},
   {key:'x',label:'X / Twitter',url:'https://x.com/trytoolscout'},
-  {key:'bluesky',label:'Bluesky',url:'https://bsky.app/profile/trytoolscout.bsky.social'}
+  {key:'bluesky',label:'Bluesky',url:'https://bsky.app/profile/trytoolscout.bsky.social'},
+  {key:'devto',label:'DEV Community',url:'https://dev.to/trytoolscout'}
 ]);
 
 function footerHtml(){
